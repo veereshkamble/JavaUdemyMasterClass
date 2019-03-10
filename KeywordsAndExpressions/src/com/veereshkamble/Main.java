baa4df01b9ce5dde0d7880f13a85136d6e128d9c
veereshkamble <veereshkamble7@gmail.com>
2019-03-10 16:59:52 -0700

    Expressions and Statements

---
+++ KeywordsAndExpressions/src/com/veereshkamble/Main.java
@@ -0,0 +1,20 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        int score = 100; //score = 100 is the expression
+        if(score > 90) { //score > 90 is the expression
+            System.out.println("You got the high score"); // "You got the high score" is the expression
+            score = 0; //score = 0 is the expression s
+        }
+    }
+}
