581114c54d8bd1ea38c5ef5418b3b2a802cab306
veereshkamble <veereshkamble7@gmail.com>
2019-03-09 20:53:33 -0800

    CharAndBoolean

---
+++ CHarAndBoolean/src/com/veereshkamble/Main.java
@@ -0,0 +1,18 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //widht of char 16 (2 bytes)
+        char myChar = '\u00A9';
+        System.out.println("My Char = " +myChar);
+
+        boolean myBoolean = true;
+        System.out.println("My Boolean = " +myBoolean);
+
+        char myRegChar = '\u00AE';
+        System.out.println("My Char = " +myRegChar );
+    }
+}
