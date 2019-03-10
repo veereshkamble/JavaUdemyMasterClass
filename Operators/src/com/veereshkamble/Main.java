a8bc3c19e00efa0072dcaa823c8e32ab57f68812
veereshkamble <veereshkamble7@gmail.com>
2019-03-09 22:11:19 -0800

    Operators again

--- Operators/src/com/veereshkamble/Main.java
+++ Operators/src/com/veereshkamble/Main.java
@@ -47,9 +47,14 @@ public class Main {
             System.out.println("It is not an alien");
         }
 
-        int topScore = 100;
+        int topScore = 80;
         if(topScore != 100) {
             System.out.println("You got the high score");
+        }
+
+        int secondTopScore = 60;
+        if(topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
         }
     }
 }
