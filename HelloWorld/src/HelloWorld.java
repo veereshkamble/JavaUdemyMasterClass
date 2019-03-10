3324539b791ba798e8753008b02a394aaa89d085
veereshkamble <veereshkamble7@gmail.com>
2019-03-09 17:05:02 -0800

    Hello Worldgit status

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = (7 + 3) + ( 6 + 4);
+        int mySecondNumber = 9;
+        int myThirdNumber = 14;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastone = myTotal +7;
+        System.out.println(myLastone);
+    }
+}
