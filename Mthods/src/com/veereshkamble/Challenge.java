e9efb236a5e7906ea089e5b84d129e35080f1255
veereshkamble <veereshkamble7@gmail.com>
2019-03-10 19:37:12 -0700

    Methods Challenge

---
+++ Mthods/src/com/veereshkamble/Challenge.java
@@ -0,0 +1,39 @@
+package com.veereshkamble;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+
+        int position = calculateHighScorePosition(1500);
+        displayHighScorePosition("Ronaldo", position);
+
+        position = calculateHighScorePosition(900);
+        displayHighScorePosition("Messi", position);
+
+        position = calculateHighScorePosition(400);
+        displayHighScorePosition("Neymar", position);
+
+        position = calculateHighScorePosition(50);
+        displayHighScorePosition("Pogba", position);
+    }
+
+    public static void displayHighScorePosition(String name, int position) {
+        System.out.println(name + " managed to get into position "
+                + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+
+        if(playerScore >= 1000) {
+            return 1;
+        } else if(playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if(playerScore >=100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+
+}
