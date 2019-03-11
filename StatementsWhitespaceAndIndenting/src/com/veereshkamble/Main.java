70a82874de54124c02ebbfb12c7d69c70cda9ca0
veereshkamble <veereshkamble7@gmail.com>
2019-03-10 17:13:49 -0700

    Statements Whitespace and Indenting

---
+++ StatementsWhitespaceAndIndenting/src/com/veereshkamble/Main.java
@@ -0,0 +1,23 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                "another" +
+                "still more");
+
+        int anotherVariable = 50; anotherVariable++;System.out.println("This is another one");
+
+        int secondVariable          =
+                    50; 
+
+       }
+}
