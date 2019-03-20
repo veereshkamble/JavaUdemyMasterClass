1582f43d471fe0a5b84c9f09da21f69820273ae6
veereshkamble <veereshkamble7@gmail.com>
2019-03-19 21:37:56 -0700

    Coding Exercises again

---
+++ CodingExercises/src/com/veereshkamble/Main.java
@@ -0,0 +1,9 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Coding Exercises begin here and now. Today!");
+    }
+}
