834a37c4b70ddda443eb455a5fc05bb6b9ab206c
veereshkamble <veereshkamble7@gmail.com>
2019-03-19 22:34:37 -0700

    Positive Negative Zero

---
+++ CodingExercises/src/com/veereshkamble/PositiveNegativeZero.java
@@ -0,0 +1,17 @@
+package com.veereshkamble;
+
+public class PositiveNegativeZero {
+
+    public static void checkNumber(int number) {
+
+        if(number > 0) {
+            System.out.println("Positive");
+        } else if(number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    
+}
