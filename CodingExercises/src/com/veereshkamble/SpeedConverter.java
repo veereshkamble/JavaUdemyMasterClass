1af9cf8078556bee2b5939cc9a62b2bf22cf8cb6
veereshkamble <veereshkamble7@gmail.com>
2019-03-25 21:47:12 -0700

    Spped Converter

--- CodingExercises/src/com/veereshkamble/SpeedConverter.java
+++ CodingExercises/src/com/veereshkamble/SpeedConverter.java
@@ -5,9 +5,8 @@ public class SpeedConverter {
 
     public static void main(String[] args) {
 
-        System.out.println(toMilesPerHour(1.5));
-        System.out.println(toMilesPerHour(-25));
-        System.out.println(toMilesPerHour(5));
+        printConversion(-28);
+        printConversion(10);
     }
 
     public static long toMilesPerHour(double kilometersPerHour) {
@@ -16,5 +15,14 @@ public class SpeedConverter {
         }
 
         return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        if(kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
     }
 }
