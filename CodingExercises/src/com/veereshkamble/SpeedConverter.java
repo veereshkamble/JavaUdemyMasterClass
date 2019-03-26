ca76c0e9b0f9a1aec8e2b1fcb6c4b97659fac929
veereshkamble <veereshkamble7@gmail.com>
2019-03-25 21:38:36 -0700

    Speed COnverter

---
+++ CodingExercises/src/com/veereshkamble/SpeedConverter.java
@@ -0,0 +1,21 @@
+package com.veereshkamble;
+
+
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(-25));
+        System.out.println(toMilesPerHour(5));
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if(kilometersPerHour < 0) {
+            return -1;
+        }
+
+        long milesPerHour = Math.round(kilometersPerHour / 1.609);
+        return milesPerHour;
+    }
+}
