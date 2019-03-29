1619baecb7e2f3805fc19db579c22949c55cdfdc
veereshkamble <veereshkamble7@gmail.com>
2019-03-28 21:38:25 -0700

    MegaBytesConverter

--- CodingExercises/src/com/veereshkamble/MegaBytesConverter.java
+++ CodingExercises/src/com/veereshkamble/MegaBytesConverter.java
@@ -4,9 +4,13 @@ public class MegaBytesConverter {
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
 
-        int mB = kiloBytes / 1024;
-        int kB = kiloBytes % 1024;
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int mB = kiloBytes / 1024;
+            int kB = kiloBytes % 1024;
 
-        System.out.println(kiloBytes + "KB = " + mB + " MB and " + kB + "KB");
+            System.out.println(kiloBytes + " KB = " + mB + " MB and " + kB + " KB");
+        }
     }
 }
