9141076520d1b0922cac26570cbed99ecf13a3a1
veereshkamble <veereshkamble7@gmail.com>
2019-04-08 21:16:05 -0700

    Monday, April 8, 2019... Deciaml Comparator

---
+++ CodingExercises/src/com/veereshkamble/DecimalComparator.java
@@ -0,0 +1,14 @@
+package com.veereshkamble;
+
+public class DecimalComparator {
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber, double secondNumber) {
+
+        if ((int)(firstNumber * 1000) == (int)(secondNumber * 1000)) {
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
