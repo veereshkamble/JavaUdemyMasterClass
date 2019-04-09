15f2406b9d90535f75b5976b5328751f9bdf7160
veereshkamble <veereshkamble7@gmail.com>
2019-04-08 21:30:50 -0700

    Monday, April 8, 2019... TeenAgeChecker

--- CodingExercises/src/com/veereshkamble/TeenNumberChecker.java
+++ CodingExercises/src/com/veereshkamble/TeenNumberChecker.java
@@ -11,4 +11,15 @@ public class TeenNumberChecker {
 
         return (age1 >=13 && age1 <= 19);
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(hasTeen(9, 99 ,19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 44));
+
+        System.out.println(isTeen(9));
+        System.out.println(isTeen(19));
+
+    }
 }
