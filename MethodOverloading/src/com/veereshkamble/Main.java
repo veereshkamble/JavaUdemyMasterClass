a808abe8cf0e292daf9ed53056a2a17e6bafa2b0
veereshkamble <veereshkamble7@gmail.com>
2019-04-09 21:50:46 -0700

    Tuesday, April 9, 2019 - Method Overloading

--- MethodOverloading/src/com/veereshkamble/Main.java
+++ MethodOverloading/src/com/veereshkamble/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int newScore = calculateScore("Veeresh", 500);
         System.out.println("New Score is " +newScore);
         calculateScore(700);
-
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -18,5 +18,10 @@ public class Main {
     public static int calculateScore(int score) {
         System.out.println("Unnamed player scored " + score  + " points.");
         return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No palyer name, no player socre");
+        return 0;
     }
 }
