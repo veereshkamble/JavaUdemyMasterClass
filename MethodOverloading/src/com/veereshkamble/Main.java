5dbe3e6919a5405f424158ac0cc836c27c2455ee
veereshkamble <veereshkamble7@gmail.com>
2019-04-09 21:44:59 -0700

    Tuesday, April 9, 2019 - Method Overloading

--- MethodOverloading/src/com/veereshkamble/Main.java
+++ MethodOverloading/src/com/veereshkamble/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int newScore = calculateScore("Veeresh", 500);
-        
+        System.out.println("New Score is " +newScore);
 
     }
 
