0f7ecbfa86eec4dc528be182e8cb8cbef1f8db30
veereshkamble <veereshkamble7@gmail.com>
2019-04-17 23:54:31 -0700

    Wednesday, April 17, 2019 - Playing cat

--- MethodOverloading/src/com/veereshkamble/PlayingCat.java
+++ MethodOverloading/src/com/veereshkamble/PlayingCat.java
@@ -18,4 +18,11 @@ public class PlayingCat {
             }
         }
     }
+
+    public static void main(String[] args) {
+
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
 }
