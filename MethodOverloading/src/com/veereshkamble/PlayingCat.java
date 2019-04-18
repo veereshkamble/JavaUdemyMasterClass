9a74158b34b1d0cb536b0a2491459c157105f51b
veereshkamble <veereshkamble7@gmail.com>
2019-04-17 23:52:57 -0700

    Wednesday, April 17, 2019 - Playing cat

--- MethodOverloading/src/com/veereshkamble/PlayingCat.java
+++ MethodOverloading/src/com/veereshkamble/PlayingCat.java
@@ -5,13 +5,13 @@ public class PlayingCat {
     public static boolean isCatPlaying(boolean summer, int temperature) {
 
         if(summer) {
-            if(temperature >= 25 && temperature <= 35) {
+            if(temperature >= 25 && temperature <= 45) {
                 return true;
             } else {
                 return false;
             }
         } else {
-            if(temperature >= 25 && temperature <= 45) {
+            if(temperature >= 25 && temperature <= 35) {
                 return true;
             } else {
                 return false;
