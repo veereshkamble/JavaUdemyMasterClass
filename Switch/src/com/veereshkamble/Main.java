1b5eaadae14f75c660ad2f7bcbdfc21b1d87e547
veereshkamble <veereshkamble7@gmail.com>
2019-04-20 19:57:53 -0700

    Saturday, April 20, 2019 - Switch

--- Switch/src/com/veereshkamble/Main.java
+++ Switch/src/com/veereshkamble/Main.java
@@ -15,7 +15,21 @@ public class Main {
             System.out.println("Value was neither 1 or 2");
         }
 
-        
+        int switchValue = 2;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            default:
+                System.out.println("Value was neither 1 or 2");
+                break;
+        }
 
     }
 }
