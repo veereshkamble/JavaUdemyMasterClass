fac8db023f7f8796383857dae7e20f5ee014658c
veereshkamble <veereshkamble7@gmail.com>
2019-04-20 19:53:40 -0700

    Saturday, April 20, 2019 - Switch

---
+++ Switch/src/com/veereshkamble/Main.java
@@ -0,0 +1,21 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int value = 1;
+
+        if(value == 1) {
+            System.out.println("Value was 1");
+        } else if(value == 2) {
+            System.out.println("value was 2");
+        } else {
+            System.out.println("Value was neither 1 or 2");
+        }
+
+        
+
+    }
+}
