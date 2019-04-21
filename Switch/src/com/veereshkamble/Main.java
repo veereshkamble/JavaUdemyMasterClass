f20a1278d53e655d0b38a9341c565c55cecb02ac
veereshkamble <veereshkamble7@gmail.com>
2019-04-20 20:05:43 -0700

    Saturday, April 20, 2019 - Switch

--- Switch/src/com/veereshkamble/Main.java
+++ Switch/src/com/veereshkamble/Main.java
@@ -15,7 +15,7 @@ public class Main {
             System.out.println("Value was neither 1 or 2");
         }
 
-        int switchValue = 2;
+        int switchValue = 3;
 
         switch(switchValue) {
             case 1:
@@ -26,8 +26,42 @@ public class Main {
                 System.out.println("Value was 2");
                 break;
 
+            case 3:case 4:case 5:
+                System.out.println("Value was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue);
+                break;
+
             default:
                 System.out.println("Value was neither 1 or 2");
+                break;
+        }
+
+        char switchAlphabet = 'A';
+
+        switch(switchAlphabet) {
+
+            case 'A':
+                System.out.println("A found");
+                break;
+
+            case 'B':
+                System.out.println("B found");
+                break;
+
+            case 'C':
+                System.out.println("C found");
+                break;
+
+            case 'D':
+                System.out.println("D found");
+                break;
+
+            case 'E':
+                System.out.println("E found");
+                break;
+
+            default:
+                System.out.println("Not found");
                 break;
         }
 
