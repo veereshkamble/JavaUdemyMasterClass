672658d1d3398df744c9bd5ae1a3fd9224bacf75
veereshkamble <veereshkamble7@gmail.com>
2019-04-22 23:38:39 -0700

    Monday, April 22, 2019 - For Loops

--- ForLoops/src/com/veereshkamble/Main.java
+++ ForLoops/src/com/veereshkamble/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 3.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 4.0));
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 5.0));
     }
 
     public static double calculateInterest(double amount, double interestRate) {
