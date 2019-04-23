30d596462d01fdd839d1832462d6f9f17f871672
veereshkamble <veereshkamble7@gmail.com>
2019-04-22 23:55:46 -0700

    Monday, April 22, 2019 - For Loops

--- ForLoops/src/com/veereshkamble/Main.java
+++ ForLoops/src/com/veereshkamble/Main.java
@@ -21,9 +21,27 @@ public class Main {
         for(int i = 8; i >= 2; i--) {
             System.out.println("10,000 at " + i + " % interest rate = " + String.format("%.2f",calculateInterest(10000,i)));
         }
+
+        System.out.println(isPrime(10));
+        System.out.println(isPrime(11));
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return(amount * (interestRate / 100));
+    }
+
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for(int i = 2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
