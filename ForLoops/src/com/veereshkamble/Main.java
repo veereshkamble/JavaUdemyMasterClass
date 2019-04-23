df2fc4a5583a612e85b1b536f40dbb2b9ed4052b
veereshkamble <veereshkamble7@gmail.com>
2019-04-22 23:37:43 -0700

    Monday, April 22, 2019 - For Loops

---
+++ ForLoops/src/com/veereshkamble/Main.java
@@ -0,0 +1,13 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("10,000 at 2% interest = " + calculateInterest(10000.0, 2.0));
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return(amount * (interestRate / 100));
+    }
+}
