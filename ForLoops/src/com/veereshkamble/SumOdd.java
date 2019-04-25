86d23e8e8b52d42d3817e3962432c9b27c4be633
veereshkamble <veereshkamble7@gmail.com>
2019-04-24 22:13:06 -0700

    Wednesday, April 24, 2019 - Sum Odd

--- ForLoops/src/com/veereshkamble/SumOdd.java
+++ ForLoops/src/com/veereshkamble/SumOdd.java
@@ -4,5 +4,14 @@ public class SumOdd {
 
     public static boolean isOdd(int number) {
 
+        if(number > 0) {
+            return false;
+        }
+
+        if(number % 2 == 0 ) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
