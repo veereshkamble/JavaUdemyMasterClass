8c6ec0c98d46991814b7e739ab505271d9b9bf3b
veereshkamble <veereshkamble7@gmail.com>
2019-04-25 22:01:09 -0700

    Thursday, April 25, 2019 - While loop

---
+++ While/src/com/veereshkamble/Main.java
@@ -0,0 +1,13 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int count = 1;
+        while(count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
+}
