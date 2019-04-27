3bb1d1dfedf33ed392dd274ef95fa8c2e2f7be03
veereshkamble <veereshkamble7@gmail.com>
2019-04-27 13:57:25 -0700

    Saturday, April 27 ,2019

--- While/src/com/veereshkamble/Main.java
+++ While/src/com/veereshkamble/Main.java
@@ -24,6 +24,18 @@ public class Main {
             System.out.println("Count value is " + count);
             count++;
         }while(count != 6);
+
+        int number = 4;
+        int finishNumber = 22;
+
+        while(number <= finishNumber) {
+            number++;
+            if(!isEvenNumber(number)) {
+                continue;
+            }
+
+            System.out.println("Even number " + number);
+        }
     }
 
     public static boolean isEvenNumber(int number) {
