5e7293a87255cd58f640ec1ff0203a45d370734d
veereshkamble <veereshkamble7@gmail.com>
2019-04-29 23:35:28 -0700

    Monday, April 29, 2019 - SharedDigit

---
+++ While/src/com/veereshkamble/SharedDigit.java
@@ -0,0 +1,22 @@
+package com.veereshkamble;
+
+public class SharedDigit {
+
+    public static boolean hasSharedDigit(int num1, int num2) {
+
+        if(num1 < 10 || num1 > 99 || num2 < 10 || num2 > 99) {
+            return false;
+        }
+
+        while(num1 != 0) {
+
+            int num1Digit = num1 % 10;
+            if(num1Digit == (num2 % 10) || num1Digit == (num2 / 10)) {
+                return true;
+            }
+            num1 /= 10;
+        }
+
+        return false;
+    }
+}
