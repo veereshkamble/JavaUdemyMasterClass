14df354f864f900e866ff5339755ab9b494fb556
veereshkamble <veereshkamble7@gmail.com>
2019-05-01 21:18:56 -0700

    Wednesday, May 1, 2019 - Greatest Common Divisor

--- While/src/com/veereshkamble/GreatestCommonDivisor.java
+++ While/src/com/veereshkamble/GreatestCommonDivisor.java
@@ -1,4 +1,22 @@
 package com.veereshkamble;
 
 public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+
+        if(first < 10 || second < 10) {
+            return -1;
+        }
+
+        int divisor = 1;
+        int greatestDivisor = 1;
+        while(divisor <= first || divisor <= second) {
+
+            if((first % divisor == 0) && (second % divisor == 0)) {
+                greatestDivisor = divisor;
+            }
+            divisor++;
+        }
+        return greatestDivisor;
+    }
 }
