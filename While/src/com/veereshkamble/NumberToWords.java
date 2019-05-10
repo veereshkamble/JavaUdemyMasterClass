ddb850a5b3a57833e203bce06afd2c00c560b7d2
veereshkamble <veereshkamble7@gmail.com>
2019-05-09 23:34:19 -0700

    Thursday, May 9, 2019 - NumberInWords

--- While/src/com/veereshkamble/NumberToWords.java
+++ While/src/com/veereshkamble/NumberToWords.java
@@ -4,17 +4,33 @@ public class NumberToWords {
 
     public static void numberToWords(int number) {
 
+        if(number < 0) {
+            System.out.println("Invalid Value");
+        }
+
         int reverseNumber = reverse(number);
-        System.out.println(reverseNumber);
+        String numberInWords = "";
+
+        while(reverseNumber != 0) {
+            int digit = reverseNumber % 10;
+            switch (digit) {
+                case 0:
+                    numberInWords = numberInWords + "Zero ";
+            }
+        }
     }
 
     public static int reverse(int number) {
+
         int reverse = 0;
+        number = Math.abs(number);
+
         while(number != 0) {
             int digit = number % 10;
             reverse = reverse * 10 + digit;
             number = number / 10;
         }
+
         return reverse;
     }
 
