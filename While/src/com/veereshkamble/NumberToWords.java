7c721628d73f298d4d82b9a462b7be2692cba12d
veereshkamble <veereshkamble7@gmail.com>
2019-05-09 23:27:02 -0700

    Thursday, May 9, 2019

--- While/src/com/veereshkamble/NumberToWords.java
+++ While/src/com/veereshkamble/NumberToWords.java
@@ -1,4 +1,24 @@
 package com.veereshkamble;
 
 public class NumberToWords {
+
+    public static void numberToWords(int number) {
+
+        int reverseNumber = reverse(number);
+        System.out.println(reverseNumber);
+    }
+
+    public static int reverse(int number) {
+        int reverse = 0;
+        while(number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number = number / 10;
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        numberToWords(234);
+    }
 }
