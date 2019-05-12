5b5d4fe1c4753e13eae15b94540396064d53dcff
veereshkamble <veereshkamble7@gmail.com>
2019-05-12 14:14:52 -0700

    Sunday, May 12, 2019 - ParseValuesFromString

---
+++ ParsingValuesFromString/src/com/veereshkamble/Main.java
@@ -0,0 +1,22 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String numberAsString = "2018";
+        System.out.println("numberAsString= " + numberAsString);
+
+        int number = Integer.parseInt(numberAsString);
+        System.out.println("numer = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString= " + numberAsString);
+        System.out.println("numer = " + number);
+
+        double dNumber = Double.parseDouble(numberAsString);
+        System.out.println("numer = " + dNumber);
+    }
+}
