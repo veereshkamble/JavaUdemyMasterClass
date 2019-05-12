a19b0bbc939d20902df948f7f997dba4b443517d
veereshkamble <veereshkamble7@gmail.com>
2019-05-12 14:53:45 -0700

    Sunday, May 12, 2019 - FlourPackProblem

--- ParsingValuesFromString/src/com/veereshkamble/FlourPackProblem.java
+++ ParsingValuesFromString/src/com/veereshkamble/FlourPackProblem.java
@@ -8,18 +8,22 @@ public class FlourPackProblem {
             return false;
         }
 
-        if(bigCount + smallCount >= goal) {
-            return true;
+        int bigKilo = 5 * bigCount;
+        int smallKilo = 1 * smallCount;
+
+        if(goal < 5) {
+            return smallKilo >= goal;
+        } else {
+            return bigKilo + smallKilo >= goal;
         }
 
-        return false;
     }
 
     public static void main(String[] args) {
         System.out.println(canPack(1, 0, 4));
         System.out.println(canPack(1, 0, 5));
         System.out.println(canPack(0, 5, 4));
-        System.out.println(canPack(2,2,11));
+        System.out.println(canPack(2, 2, 11));
         System.out.println(canPack(-3,2, 12));
     }
 }
