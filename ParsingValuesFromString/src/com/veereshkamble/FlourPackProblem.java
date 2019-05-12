0d1a5ebd3c1d6bb2549b98a0da48b83313b4e1a3
veereshkamble <veereshkamble7@gmail.com>
2019-05-12 16:17:21 -0700

    Sunday, May 12, 2019 - Flour Pack Problem - done

--- ParsingValuesFromString/src/com/veereshkamble/FlourPackProblem.java
+++ ParsingValuesFromString/src/com/veereshkamble/FlourPackProblem.java
@@ -8,21 +8,23 @@ public class FlourPackProblem {
             return false;
         }
 
-        if(bigCount == 0) {
-            return smallCount >= goal;
-        } else {
-            int bigCountUsed = goal / 5;
-            iint remainingGoal = goal - (bigCountUsed * 5);
-            if(bigCountUsed == 0) {
-                return smallCount >= goal;
-            } else {
+        int total = bigCount * 5 + smallCount;
 
+        if(total < goal) {
+            return  false;
+        } else if(total == goal) {
+            return true;
+        } else {
+            int bigCountReq = goal / 5;
+            if(bigCount <= bigCountReq) {
+                int remainingGoal = goal - (bigCount * 5);
+                return smallCount >= remainingGoal;
+            } else {
+                int remainingGoal = goal - (bigCountReq * 5);
                 return smallCount >= remainingGoal;
             }
+
         }
-
-
-
 
     }
 
@@ -33,5 +35,7 @@ public class FlourPackProblem {
         System.out.println(canPack(2, 2, 11));
         System.out.println(canPack(-3,2, 12));
         System.out.println(canPack(5, 3, 24));
+        System.out.println(canPack(0, 5, 6));
+        System.out.println(canPack(2, 7, 18));
     }
 }
