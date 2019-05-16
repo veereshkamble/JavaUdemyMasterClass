320d19ff4e93d0d2c281befa298ab2f8d8368caf
veereshkamble <veereshkamble7@gmail.com>
2019-05-15 23:49:22 -0700

    Wednesday, May 15, 2019 - Paint Job - getBucketCOunt

--- ReadingUserInput/src/com/veereshkamble/PaintJob.java
+++ ReadingUserInput/src/com/veereshkamble/PaintJob.java
@@ -2,5 +2,12 @@ package com.veereshkamble;
 
 public class PaintJob {
 
+    public static int getBucketCount(double width, double height, int areaPerBucket, int extraBuckets) {
+
+        if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+    }
+
 
 }
