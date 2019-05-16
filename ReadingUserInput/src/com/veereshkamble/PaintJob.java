6ea6d15a988410872dc77227c21c82a6f3f0c3d0
veereshkamble <veereshkamble7@gmail.com>
2019-05-15 23:58:46 -0700

    Wednesday, May 15, 2019 - Paint Job - getBucketCOunt

--- ReadingUserInput/src/com/veereshkamble/PaintJob.java
+++ ReadingUserInput/src/com/veereshkamble/PaintJob.java
@@ -2,11 +2,15 @@ package com.veereshkamble;
 
 public class PaintJob {
 
-    public static int getBucketCount(double width, double height, int areaPerBucket, int extraBuckets) {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
 
         if(width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
             return -1;
         }
+
+        double area = width * height;
+        int totalBuckets = (int) (area / areaPerBucket);
+        return totalBuckets - extraBuckets;
     }
 
 
