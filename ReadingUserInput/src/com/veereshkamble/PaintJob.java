8fcb9e2773bcebfec57065b34fb2ff76706824ad
veereshkamble <veereshkamble7@gmail.com>
2019-05-16 21:12:44 -0700

    Thursday, May 16, 2019 - Paint Job

--- ReadingUserInput/src/com/veereshkamble/PaintJob.java
+++ ReadingUserInput/src/com/veereshkamble/PaintJob.java
@@ -39,7 +39,9 @@ public class PaintJob {
         System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
         System.out.println(getBucketCount(3.4, 2.1, 1.5));
         System.out.println(getBucketCount(7.25, 4.3, 2.35));
-        
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
     }
 
 }
