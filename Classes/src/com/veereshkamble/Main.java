0d87426ab7f06369da82246c1f14d0327b26540b
veereshkamble <veereshkamble7@gmail.com>
2019-06-04 21:39:15 -0700

    Tuesday, June 04, 2019 - Classes - Fields

--- Classes/src/com/veereshkamble/Main.java
+++ Classes/src/com/veereshkamble/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Car porsche = new Car();
+        Car maruti = new Car();
+
+        porsche.
     }
 }
