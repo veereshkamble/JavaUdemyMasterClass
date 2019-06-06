f8945bd5cd19639890b57f5213f5db9a8dcf06dd
veereshkamble <veereshkamble7@gmail.com>
2019-06-05 23:46:34 -0700

    Wednesday, June 5, 2019 - Sum Calculator

---
+++ Classes/src/com/veereshkamble/SumCalculator.java
@@ -0,0 +1,25 @@
+package com.veereshkamble;
+
+public class SumCalculator {
+
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+
+}
