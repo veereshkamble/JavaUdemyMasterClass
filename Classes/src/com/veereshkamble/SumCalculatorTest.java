b73e193d83eb13af245460d7bd20b16477616a52
veereshkamble <veereshkamble7@gmail.com>
2019-06-05 23:55:57 -0700

    Wednesday, June 5, 2019 - Sum Calculator

--- Classes/src/com/veereshkamble/SumCalculatorTest.java
+++ Classes/src/com/veereshkamble/SumCalculatorTest.java
@@ -10,6 +10,10 @@ public class SumCalculatorTest {
         sumCalculator.setSecondNumber(4);
         System.out.println("add= " +sumCalculator.getAdditionResult());
         System.out.println("subtract= " +sumCalculator.getSubtractionResult());
-        
+        sumCalculator.setFirstNumber(5.25);
+        sumCalculator.setSecondNumber(0);
+        System.out.println("multiply= " + sumCalculator.getMultiplicationResult());
+        System.out.println("divide= " + sumCalculator.getDivisionResult());
+
     }
 }
