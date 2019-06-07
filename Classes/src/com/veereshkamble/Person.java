d7ed122eb96e989f5b61de76f508d5b023d3441f
veereshkamble <veereshkamble7@gmail.com>
2019-06-06 23:45:38 -0700

    Thursday, June 6, 2019 - Person class

---
+++ Classes/src/com/veereshkamble/Person.java
@@ -0,0 +1,23 @@
+package com.veereshkamble;
+
+public class Person {
+
+    private  String firstName;
+    private  String lastName;
+    private  int age;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+
+    
+}
