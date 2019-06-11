112ba09ec503250db5063e41e55ba4f52b8f592a
veereshkamble <veereshkamble7@gmail.com>
2019-06-10 22:54:39 -0700

    Monday, June 10, 2019- Bank Account - getters and setters

--- Classes/src/com/veereshkamble/BankAccount.java
+++ Classes/src/com/veereshkamble/BankAccount.java
@@ -8,7 +8,7 @@ public class BankAccount {
     private String email;
     private int phoneNumber;
 
-    public void setAccountNumber(int accountNumber) {
+    public void setAccountNumber(long accountNumber) {
         this.accountNumber = accountNumber;
     }
 
@@ -16,5 +16,35 @@ public class BankAccount {
         return this.accountNumber;
     }
 
-    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getPhoneNumber() {
+        return this.phoneNumber;
+    }
 }
