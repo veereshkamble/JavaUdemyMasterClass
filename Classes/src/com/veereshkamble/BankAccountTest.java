ea045741c8bff8e93312e2e13397fa17a30813f7
veereshkamble <veereshkamble7@gmail.com>
2019-06-10 23:04:57 -0700

    Monday, June 10, 2019- Bank Account - deposit Funds and withdraw funds

---
+++ Classes/src/com/veereshkamble/BankAccountTest.java
@@ -0,0 +1,30 @@
+package com.veereshkamble;
+
+public class BankAccountTest {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+
+        bankAccount.setAccountNumber(12345);
+        bankAccount.setBalance(1000);
+        bankAccount.setCustomerName("Ronaldo");
+        bankAccount.setEmail("cristiano@ronaldo.com");
+        bankAccount.setPhoneNumber(7);
+
+        System.out.println("Account Number : " +bankAccount.getAccountNumber());
+        System.out.println("Balance : " +bankAccount.getBalance());
+        System.out.println("Name : " +bankAccount.getCustomerName());
+        System.out.println("Email : " +bankAccount.getEmail());
+        System.out.println("Phone Number : " +bankAccount.getPhoneNumber());
+
+        bankAccount.depositFunds(500.00);
+        System.out.println("New Balance : " +bankAccount.getBalance());
+
+        bankAccount.withdrawFinds(500.00);
+        System.out.println("New Balance : " +bankAccount.getBalance());
+
+        bankAccount.withdrawFinds(11000.00);
+        System.out.println("New Balance : " +bankAccount.getBalance());
+    }
+}
