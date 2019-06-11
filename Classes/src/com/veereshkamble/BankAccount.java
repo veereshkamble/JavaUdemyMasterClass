7e2ff9250be9ac18d14ffe56ffecbc767e8a96fc
veereshkamble <veereshkamble7@gmail.com>
2019-06-10 22:50:46 -0700

    Monday, June 10, 2019- Bank Account

---
+++ Classes/src/com/veereshkamble/BankAccount.java
@@ -0,0 +1,20 @@
+package com.veereshkamble;
+
+public class BankAccount {
+
+    private long accountNumber;
+    private double balance;
+    private String customerName;
+    private String email;
+    private int phoneNumber;
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public long getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    
+}
