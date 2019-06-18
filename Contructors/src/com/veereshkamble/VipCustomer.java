b8a943202057c9458da18c73d87a3d9133753b9c
veereshkamble <veereshkamble7@gmail.com>
2019-06-17 23:10:15 -0700

    Monday, June 17, 2019 - Vip Customer

---
+++ Contructors/src/com/veereshkamble/VipCustomer.java
@@ -0,0 +1,20 @@
+package com.veereshkamble;
+
+public class VipCustomer {
+
+    private String name;
+    private double crediteLimit;
+    private String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCrediteLimit() {
+        return crediteLimit;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+}
