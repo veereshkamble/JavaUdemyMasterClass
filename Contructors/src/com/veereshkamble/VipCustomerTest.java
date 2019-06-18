213a95d217c5b88679040dc52c66a4ed2d50c9ed
veereshkamble <veereshkamble7@gmail.com>
2019-06-17 23:22:14 -0700

    Monday, June 17, 2019 - Vip Customer

--- Contructors/src/com/veereshkamble/VipCustomerTest.java
+++ Contructors/src/com/veereshkamble/VipCustomerTest.java
@@ -4,6 +4,13 @@ public class VipCustomerTest {
 
     public static void main(String[] args) {
 
-        
+        VipCustomer vipCustomer1 = new VipCustomer();
+        System.out.println(vipCustomer1.getName());
+
+        VipCustomer vipCustomer2 = new VipCustomer("bob", "bob@gmail.com");
+        System.out.println(vipCustomer2.getName());
+
+        VipCustomer vipCustomer3 = new VipCustomer("Time", 50000, "tim@gmail.com");
+        System.out.println(vipCustomer3.getName());
     }
 }
