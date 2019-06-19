89b70cc84b7373ccb6535ad09e597311073d05ec
veereshkamble <veereshkamble7@gmail.com>
2019-06-18 23:25:44 -0700

    Tuesday, June 18, 2019

---
+++ Contructors/src/com/veereshkamble/WallTest.java
@@ -0,0 +1,12 @@
+package com.veereshkamble;
+
+public class WallTest {
+
+    public static void main(String[] args) {
+
+        Wall wall = new Wall(5, 4);
+        System.out.println("Area = " + wall.getArea());
+
+        
+    }
+}
