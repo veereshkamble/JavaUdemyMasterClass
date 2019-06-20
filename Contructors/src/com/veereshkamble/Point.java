2f409d2b077806ff1b2d02f73dac5586a3e59763
veereshkamble <veereshkamble7@gmail.com>
2019-06-19 23:40:48 -0700

    Wednesday, June 19, 2019 - Point

--- Contructors/src/com/veereshkamble/Point.java
+++ Contructors/src/com/veereshkamble/Point.java
@@ -5,5 +5,13 @@ public class Point {
     private int x;
     private int y;
 
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     
 }
