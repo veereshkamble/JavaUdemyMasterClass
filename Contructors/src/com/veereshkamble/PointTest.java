e9df7f8afb703fb419f2a7b6180c30d929f72bfb
veereshkamble <veereshkamble7@gmail.com>
2019-06-24 23:10:35 -0700

    Monday, June 24, 2019 - Point Test

--- Contructors/src/com/veereshkamble/PointTest.java
+++ Contructors/src/com/veereshkamble/PointTest.java
@@ -12,7 +12,9 @@ public class PointTest {
         System.out.println("distance(0,0) = " + first.distance());
         System.out.println("distance(second) = " + first.distance(second));
         System.out.println("distance(2,2) = " + first.distance(2, 2));
-        
+
+        Point point = new Point();
+        System.out.println("distance() = " + point.distance());
     }
 
 
