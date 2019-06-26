550a8261bed68f1a9b1aa40b4393259bfe86b193
veereshkamble <veereshkamble7@gmail.com>
2019-06-25 23:31:23 -0700

    Tuesday, June 25, 2019 - Floor Carpet Calculator

---
+++ Contructors/src/com/veereshkamble/Calculator.java
@@ -0,0 +1,12 @@
+package com.veereshkamble;
+
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+}
