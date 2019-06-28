27d95798b6fdaab30a830229fa60526a732de63c
veereshkamble <veereshkamble7@gmail.com>
2019-06-27 23:46:02 -0700

    Thursday, June 27, 2019

--- Contructors/src/com/veereshkamble/CalculatorTest.java
+++ Contructors/src/com/veereshkamble/CalculatorTest.java
@@ -2,6 +2,14 @@ package com.veereshkamble;
 
 public class CalculatorTest {
 
-    Carpet carpet = new Carpet(3.5);
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("Total = " + calculator.getTotalCost());
+
+        
+    }
+
 
 }
