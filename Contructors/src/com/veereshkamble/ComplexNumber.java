0059caf9b9c8f5c31235d4f6b37617b035ad34c8
veereshkamble <veereshkamble7@gmail.com>
2019-06-27 23:55:42 -0700

    Thursday, June 27, 2019 - Complex Number

---
+++ Contructors/src/com/veereshkamble/ComplexNumber.java
@@ -0,0 +1,14 @@
+package com.veereshkamble;
+
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    
+}
