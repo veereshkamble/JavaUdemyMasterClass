997340ef6d02c1626257a33e9f1f130af8ddd13e
veereshkamble <veereshkamble7@gmail.com>
2019-07-30 23:25:48 -0700

    Tuesday, July 30, 2019 - Complex Number Test

--- Contructors/src/com/veereshkamble/ComplexNumberTest.java
+++ Contructors/src/com/veereshkamble/ComplexNumberTest.java
@@ -4,5 +4,12 @@ public class ComplexNumberTest {
 
     public static void main(String[] args) {
 
+        ComplexNumber one = new ComplexNumber(1.0, 1.0);
+        ComplexNumber number = new ComplexNumber(2.5, -1.5);
+        one.add(1, 1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+
+        
     }
 }
