8ec669ce06385041cacc9fb0d2078ec13a926597
veereshkamble <veereshkamble7@gmail.com>
2019-08-05 20:55:42 -0700

    Monday, August 5, 2019 - Inheritance

---
+++ Inheritance/src/com/veereshkamble/Animal.java
@@ -0,0 +1,64 @@
+package com.veereshkamble;
+
+/**
+ * Created by Vee on Aug 5, 2019
+ */
+public class Animal {
+
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+
+    public Animal() {
+    }
+
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBrain(int brain) {
+        this.brain = brain;
+    }
+
+    public void setBody(int body) {
+        this.body = body;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
