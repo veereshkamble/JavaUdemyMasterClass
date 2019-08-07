ca0a94ceaf3d8a7dde89924846f25c1866e50e03
veereshkamble <veereshkamble7@gmail.com>
2019-08-06 21:41:18 -0700

    Monday, August 6, 2019 - Inheritance CHallenge. Camaro class

--- Inheritance/src/com/veereshkamble/Camaro.java
+++ Inheritance/src/com/veereshkamble/Camaro.java
@@ -5,7 +5,28 @@ public class Camaro extends Car {
     private int roadService;
 
     public Camaro(int roadService) {
-        super("Camaro", "4WD", 4, 2, 8, true);
+        super("Camaro", "4WD", 4, 2, 8, false);
         this.roadService = roadService;
+    }
+
+    public void accelerate(int rate) {
+
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0) {
+            stop();
+            changeGear(1);
+        } else if(newVelocity > 0 && newVelocity <= 10) {
+            changeGear(1);
+        } else if(newVelocity > 10 && newVelocity <= 20) {
+            changeGear(2);
+        } else if(newVelocity > 20 && newVelocity <= 30) {
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+
+        if(newVelocity > 0) {
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
     }
 }
