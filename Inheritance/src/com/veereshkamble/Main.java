6a106febdd711b5fbd44470faad4a7b2cf63f046
veereshkamble <veereshkamble7@gmail.com>
2019-08-06 21:46:15 -0700

    Monday, August 6, 2019 - Inheritance CHallenge. Camaro class

--- Inheritance/src/com/veereshkamble/Main.java
+++ Inheritance/src/com/veereshkamble/Main.java
@@ -5,12 +5,18 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+        /*Animal animal = new Animal("Animal", 1, 1, 5, 5);
 
         Dog dog = new  Dog("Yorkie", 8, 20, 2, 4, 1, 20, "Long Silky");
 
         dog.eat();
         dog.walk();
-        dog.run();
+        dog.run();*/
+
+        Camaro camaro = new Camaro(36);
+        camaro.steer(45);
+        camaro.accelerate(30);
+        camaro.accelerate(20);
+        camaro.accelerate(-43);
     }
 }
