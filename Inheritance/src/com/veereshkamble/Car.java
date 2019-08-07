7b05c68bcdc36f4c511725d8a788e701f1922e17
veereshkamble <veereshkamble7@gmail.com>
2019-08-06 21:29:32 -0700

    Monday, August 6, 2019 - Inheritance CHallenge. Car class

--- Inheritance/src/com/veereshkamble/Car.java
+++ Inheritance/src/com/veereshkamble/Car.java
@@ -2,5 +2,65 @@ package com.veereshkamble;
 
 public class Car extends Vehicle{
 
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManual;
 
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public void changeGear(int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
+
+    public void changeVelocity(int speed, int direction) {
+        move(speed, direction);
+        System.out.println("Car.changeVelocity() : Velocity " + speed + " direction " + direction);
+    }
 }
