ed3c8be4f7a4407f3a05991d9b38b8175fa53847
veereshkamble <veereshkamble7@gmail.com>
2019-08-08 23:11:56 -0700

    Thursday, August 8, 2019 - Rectangle

---
+++ Inheritance/src/com/veereshkamble/Rectangle.java
@@ -0,0 +1,23 @@
+package com.veereshkamble;
+
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if(width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+        if(length < 0) {
+            this.length = 0;
+        } else {
+            this.length = length;
+        }
+    }
+
+    
+}
