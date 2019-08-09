9332262e002c2db78e5c045eaaa79128ac9a3579
veereshkamble <veereshkamble7@gmail.com>
2019-08-08 23:16:27 -0700

    Thursday, August 8, 2019 - Cuboid

---
+++ Inheritance/src/com/veereshkamble/Cuboid.java
@@ -0,0 +1,24 @@
+package com.veereshkamble;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+
+        if(this.height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * getHeight();
+    }
+}
