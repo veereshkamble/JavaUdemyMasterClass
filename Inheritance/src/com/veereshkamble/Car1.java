28d4e5fe356387cc2501680f7b38a58e878b3f2e
veereshkamble <veereshkamble7@gmail.com>
2019-08-12 23:19:01 -0700

    Monday, August 12, 2019

---
+++ Inheritance/src/com/veereshkamble/Car1.java
@@ -0,0 +1,4 @@
+package com.veereshkamble;
+
+public class Car1 extends Vehicle1 {
+}
---
+++ Inheritance/src/com/veereshkamble/Vehicle1.java
@@ -0,0 +1,10 @@
+package com.veereshkamble;
+
+public class Vehicle1 {
+
+    private String name;
+
+    public Vehicle1(String name) {
+        this.name = name;
+    }
+}
