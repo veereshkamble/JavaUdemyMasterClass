3e42d6f0928b34f689b9cc0563871b2962661e9c
veereshkamble <veereshkamble7@gmail.com>
2019-08-13 22:27:55 -0700

    Tuesday, August 31, 2019- Composition Bedroom

--- Composition/src/Bedroom.java
+++ Composition/src/Bedroom.java
@@ -20,5 +20,71 @@ public class Bedroom {
         this.lamp = lamp;
     }
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public Wall getWall1() {
+        return wall1;
+    }
+
+    public Wall getWall2() {
+        return wall2;
+    }
+
+    public Wall getWall3() {
+        return wall3;
+    }
+
+    public Wall getWall4() {
+        return wall4;
+    }
+
+    public Ceiling getCeiling() {
+        return ceiling;
+    }
+
+    public Bed getBed() {
+        return bed;
+    }
+
+    public Lamp getLamp() {
+        return lamp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWall1(Wall wall1) {
+        this.wall1 = wall1;
+    }
+
+    public void setWall2(Wall wall2) {
+        this.wall2 = wall2;
+    }
+
+    public void setWall3(Wall wall3) {
+        this.wall3 = wall3;
+    }
+
+    public void setWall4(Wall wall4) {
+        this.wall4 = wall4;
+    }
+
+    public void setCeiling(Ceiling ceiling) {
+        this.ceiling = ceiling;
+    }
+
+    public void setBed(Bed bed) {
+        this.bed = bed;
+    }
+
+    public void setLamp(Lamp lamp) {
+        this.lamp = lamp;
+    }
+
+    public void makeBed() {
+        System.out.println("Bedroom -> Making Bed");
+    }
 }
