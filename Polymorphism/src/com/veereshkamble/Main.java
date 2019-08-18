2a0af903409e1bebfd497293a214fd442526c5db
veereshkamble <veereshkamble7@gmail.com>
2019-08-17 20:06:57 -0700

    Satruday, August 17, 2019 - Polymorphism Challenge

--- Polymorphism/src/com/veereshkamble/Main.java
+++ Polymorphism/src/com/veereshkamble/Main.java
@@ -10,6 +10,10 @@ class Movie {
     public String plot() {
         return "No plot here";
     }
+
+    public String getName() {
+        return name;
+    }
 }
 
 class Jaws extends Movie {
@@ -68,7 +72,32 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        for(int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i + ": " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
+        }
+    }
 
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("random number generated was: " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
 
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new StarWars();
+
+            case 5:
+                return new Forgettable();
+        }
+        return null;
     }
 }
