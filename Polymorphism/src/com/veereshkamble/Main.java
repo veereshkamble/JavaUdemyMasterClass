c45f00747f03be1cbdc644616498e6662aaf4b1e
veereshkamble <veereshkamble7@gmail.com>
2019-08-17 19:30:03 -0700

    Satruday, August 17, 2019 - Polymorphism

---
+++ Polymorphism/src/com/veereshkamble/Main.java
@@ -0,0 +1,23 @@
+package com.veereshkamble;
+
+class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No plot here";
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+    }
+}
