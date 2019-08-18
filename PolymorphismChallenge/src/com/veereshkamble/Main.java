50e43ee26558e9b722d6489b4c313a137c690baa
veereshkamble <veereshkamble7@gmail.com>
2019-08-17 20:11:07 -0700

    Satruday, August 17, 2019 - Polymorphism Challenge

--- PolymorphismChallenge/src/com/veereshkamble/Main.java
+++ PolymorphismChallenge/src/com/veereshkamble/Main.java
@@ -1,8 +1,54 @@
 package com.veereshkamble;
 
+class Cars {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Cars(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
     }
 }
