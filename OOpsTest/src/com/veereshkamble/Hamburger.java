b7fc250937ba33f860ac0fc35df872db9c7f534e
veereshkamble <veereshkamble7@gmail.com>
2019-08-18 19:43:18 -0700

    Sunday,August 18, 2019 - Master OOPs challenge

--- OOpsTest/src/com/veereshkamble/Hamburger.java
+++ OOpsTest/src/com/veereshkamble/Hamburger.java
@@ -46,5 +46,31 @@ public class Hamburger {
         this.addition4Price = price;
     }
 
-    
+    public double itemizeHamburger() {
+        double hamburgerPrice = this.price;
+        System.out.println(this.name + " hamburger " + " on a " + this.breadRollType + " roll "
+        + " price is " + this.price);
+
+        if(this.addition1Name != null) {
+            hamburgerPrice += this.addition1Price;
+            System.out.println("Added " + this.addition1Name + " for an extra " + this.addition1Price);
+        }
+
+        if(this.addition2Name != null) {
+            hamburgerPrice += this.addition2Price;
+            System.out.println("Added " + this.addition2Name + " for an extra " + this.addition2Price);
+        }
+
+        if(this.addition3Name != null) {
+            hamburgerPrice += this.addition3Price;
+            System.out.println("Added " + this.addition3Name + " for an extra " + this.addition3Price);
+        }
+
+        if(this.addition4Name != null) {
+            hamburgerPrice += this.addition4Price;
+            System.out.println("Added " + this.addition4Name + " for an extra " + this.addition4Price);
+        }
+
+        
+    }
 }
