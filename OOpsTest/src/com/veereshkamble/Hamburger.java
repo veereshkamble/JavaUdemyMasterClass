937f92144fefa4c123ff647161ab7118e3afd551
veereshkamble <veereshkamble7@gmail.com>
2019-08-18 19:38:02 -0700

    Sunday,August 18, 2019 - Master OOPs challenge

---
+++ OOpsTest/src/com/veereshkamble/Hamburger.java
@@ -0,0 +1,50 @@
+package com.veereshkamble;
+
+public class Hamburger {
+
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+    private String addition1Name;
+    private double addition1Price;
+
+    private String addition2Name;
+    private double addition2Price;
+
+    private String addition3Name;
+    private double addition3Price;
+
+    private String addition4Name;
+    private double addition4Price;
+
+    public Hamburger(String name, String meat, double price, String breadRollType) {
+        this.name = name;
+        this.meat = meat;
+        this.price = price;
+        this.breadRollType = breadRollType;
+    }
+
+    public void addHamburgerAddition1(String name, double price) {
+        this.addition1Name = name;
+        this.addition1Price = price;
+    }
+
+    public void addHamburgerAddition2(String name, double price) {
+        this.addition2Name = name;
+        this.addition2Price = price;
+    }
+
+    public void addHamburgerAddition3(String name, double price) {
+        this.addition3Name = name;
+        this.addition3Price = price;
+    }
+
+    public void addHamburgerAddition4(String name, double price) {
+        this.addition4Name = name;
+        this.addition4Price = price;
+    }
+
+    
+}
---
+++ OOpsTest/src/com/veereshkamble/Main.java
@@ -0,0 +1,9 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
