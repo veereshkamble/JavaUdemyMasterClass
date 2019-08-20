7807e66a2fdbcb8d774ba311df34552dbdd8e22a
veereshkamble <veereshkamble7@gmail.com>
2019-08-19 21:17:58 -0700

    Monday, August 19, 2019 - Arrays

--- Arrays/src/com/veereshkamble/Main.java
+++ Arrays/src/com/veereshkamble/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] myIntArray = new int[10];
+        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        myIntArray[0] = 45;
+        myIntArray[1] = 476;
+        myIntArray[5] = 50;
 
+        double[] myDoubleArray = new double[10];
+        System.out.println(myIntArray[5]);
     }
 }
