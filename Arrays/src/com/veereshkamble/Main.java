2079b2112963a4fd2aa7a9079bfb7b6087ce38a8
veereshkamble <veereshkamble7@gmail.com>
2019-08-19 21:33:36 -0700

    Monday, August 19, 2019 - Arrays

--- Arrays/src/com/veereshkamble/Main.java
+++ Arrays/src/com/veereshkamble/Main.java
@@ -1,13 +1,37 @@
 package com.veereshkamble;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
-        int[] myIntArray = new int[25]; /*{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] myIntegers = getIntegers(5);
+        for(int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
+
+    }
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + "integer values. \r");
+        int[] values = new int[number];
+
+        for(int i =0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
+    }
+}
+
+
+	/*// write your code here
+        int[] myIntArray = new int[25]; *//*{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         myIntArray[0] = 45;
         myIntArray[1] = 476;
-        myIntArray[5] = 50;*/
+        myIntArray[5] = 50;*//*
 
         double[] myDoubleArray = new double[10];
         System.out.println(myIntArray[5]);
@@ -23,5 +47,6 @@ public class Main {
         for(int i = 0; i < myIntArray.length; i++) {
             System.out.println("Element " + i + ", value is " + myIntArray[i]);
         }
-    }
-}
+    }*/
+
+
