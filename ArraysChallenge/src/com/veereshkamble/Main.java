7a9f3345de69119f625474935243ffaf4c631358
veereshkamble <veereshkamble7@gmail.com>
2019-08-19 23:20:55 -0700

    Monday, August 19, 2019 - Arrays Challenge

--- ArraysChallenge/src/com/veereshkamble/Main.java
+++ ArraysChallenge/src/com/veereshkamble/Main.java
@@ -32,15 +32,25 @@ public class Main {
     }
 
     public static int[] sortArray(int[] array) {
+        int[] sortedArray = new int[array.length];
         for(int i = 0; i < array.length; i++) {
-            for(int j = i + 1; j < array.length - i; j++) {
-                if(array[i] < array[j]) {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+            sortedArray[i] = array[i];
+        }
+
+        boolean flag = true;
+        int temp;
+
+        while(flag) {
+            flag = false;
+            for(int i = 0; i < sortedArray.length - 1; i++) {
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
                 }
             }
         }
-        return  array;
+        return sortedArray;
     }
 }
