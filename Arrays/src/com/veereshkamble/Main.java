40b95fdc6cfe07ede824fbb6205e2eb87c8ce7e7
veereshkamble <veereshkamble7@gmail.com>
2019-08-19 21:22:42 -0700

    Monday, August 19, 2019 - Arrays

--- Arrays/src/com/veereshkamble/Main.java
+++ Arrays/src/com/veereshkamble/Main.java
@@ -4,12 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] myIntArray = new int[25]; /*{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         myIntArray[0] = 45;
         myIntArray[1] = 476;
-        myIntArray[5] = 50;
+        myIntArray[5] = 50;*/
 
         double[] myDoubleArray = new double[10];
         System.out.println(myIntArray[5]);
+
+        for(int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+
+        for(int i = 0; i < myIntArray.length; i++) {
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
     }
 }
