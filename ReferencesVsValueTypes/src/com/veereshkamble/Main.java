39801f4110ed4b92139b35112e680500b5cbd83d
veereshkamble <veereshkamble7@gmail.com>
2019-08-20 20:45:56 -0700

    Tuesday, August 20, 2019

--- ReferencesVsValueTypes/src/com/veereshkamble/Main.java
+++ ReferencesVsValueTypes/src/com/veereshkamble/Main.java
@@ -1,8 +1,11 @@
 package com.veereshkamble;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+
         int myIntValue = 10;
         int anotherIntValue = myIntValue;
 
@@ -13,8 +16,27 @@ public class Main {
 
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("anotherIntValue = " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray = " + Arrays.toString(anotherArray));
+
+        anotherArray[0] = 1;
+
+        System.out.println("after change myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray = " + Arrays.toString(anotherArray));
+
+        modifiedArray(myIntArray);
+
+        System.out.println("after modify myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("after modify anotherArray = " + Arrays.toString(anotherArray));
+
     }
 
+    private static void modifiedArray(int[] array) {
 
-
+        array[0] = 2;
+    }
 }
