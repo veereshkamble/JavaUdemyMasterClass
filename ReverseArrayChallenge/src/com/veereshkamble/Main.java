ac3c37b39f0bc4e653ab89b9334c906c023d1bb3
veereshkamble <veereshkamble7@gmail.com>
2019-08-20 21:49:14 -0700

    Tuesday, August 20, 2019

--- ReverseArrayChallenge/src/com/veereshkamble/Main.java
+++ ReverseArrayChallenge/src/com/veereshkamble/Main.java
@@ -19,7 +19,7 @@ public class Main {
     private static void reverse(int[] array) {
 
         int maxIndex = array.length - 1;
-        int halfLength = array.length;
+        int halfLength = array.length / 2;
         for(int i = 0; i < halfLength; i++) {
             int temp = array[i];
             array[i] = array[maxIndex - i];
