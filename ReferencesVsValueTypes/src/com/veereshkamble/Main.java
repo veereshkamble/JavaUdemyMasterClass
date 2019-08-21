4356a782df9004c9b625e854dac1c6c4242d6c79
veereshkamble <veereshkamble7@gmail.com>
2019-08-20 20:33:02 -0700

    Tuesday, August 20, 2019

---
+++ ReferencesVsValueTypes/src/com/veereshkamble/Main.java
@@ -0,0 +1,20 @@
+package com.veereshkamble;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+    }
+
+
+
+}
