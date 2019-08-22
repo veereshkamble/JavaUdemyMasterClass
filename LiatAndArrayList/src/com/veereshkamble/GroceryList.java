13dba650547371367eb695016bb3e18b2080f081
veereshkamble <veereshkamble7@gmail.com>
2019-08-21 23:42:14 -0700

    Wednesday, August 21, 2019 - ListAndArrayList

--- LiatAndArrayList/src/com/veereshkamble/GroceryList.java
+++ LiatAndArrayList/src/com/veereshkamble/GroceryList.java
@@ -6,5 +6,12 @@ public class GroceryList {
 
     private ArrayList<String> groceryList = new ArrayList<String>();
 
-    
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list");
+        
+    }
 }
