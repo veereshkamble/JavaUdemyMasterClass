5202a7f7f1a0174a8deb25ae0da8308d845d7540
veereshkamble <veereshkamble7@gmail.com>
2019-08-22 21:49:22 -0700

    Thursday, Aug 22, 2019

--- LiatAndArrayList/src/com/veereshkamble/Main.java
+++ LiatAndArrayList/src/com/veereshkamble/Main.java
@@ -1,9 +1,25 @@
 package com.veereshkamble;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
 
     public static void main(String[] args) {
 	// write your code here
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while(!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (choice) {
+
+            }
+        }
     }
 }
