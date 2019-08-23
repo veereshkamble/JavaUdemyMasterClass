0817cb65476ec6487891cdb6f3935b254e248612
veereshkamble <veereshkamble7@gmail.com>
2019-08-22 21:55:52 -0700

    Thursday, Aug 22, 2019 - Grocery List

--- LiatAndArrayList/src/com/veereshkamble/Main.java
+++ LiatAndArrayList/src/com/veereshkamble/Main.java
@@ -18,8 +18,47 @@ public class Main {
             scanner.nextLine();
 
             switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
 
+                case 1:
+                    groceryList.printGroceryList();;
+                    break;
+
+                case 2:
+                    addItem();
+                    break;
+
+                case 3:
+                    modifyItem();
+                    break;
+
+                case 4:
+                    removeItem();
+                    break;
+
+                case 5:
+                    searchForItem();
+                    break;
+
+                case 6:
+                    quit = true;
+                    break;
             }
         }
     }
+
+    public static void printInstructions() {
+        System.out.println("\n Choose an option ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 1 - To add an item to the list.");
+        System.out.println("\t 1 - To modify an item in the list.");
+        System.out.println("\t 1 - To remove an item from the list.");
+        System.out.println("\t 1 - To search for an item in the list.");
+        System.out.println("\t 1 - To quit the application.");
+    }
+
+    
 }
