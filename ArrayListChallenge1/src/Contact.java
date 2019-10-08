f98644b5e42deb1483f769ca123f7076a7df6c10
veereshkamble <veereshkamble7@gmail.com>
2019-10-07 23:27:44 -0700

    ArrayChallene1 - Contacts

---
+++ ArrayListChallenge1/src/Contact.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Veeresh Kamble on 07/10/2019
+ */
+
+public class Contact {
+
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public static Contact createContact(String name, String phoneNumber) {
+        return new Contact(name, phoneNumber);
+    }
+}
